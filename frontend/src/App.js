83a279e3d054ca3e0106a9428e6499295993ffb8
Chacko <chackop@gmail.com>
2020-09-18 22:41:56 +0100

    navbar, css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ArticlesList from './pages/ArticlesList';
 import ArticlePage from './pages/ArticlePage';
+import NavBar from './NavBar';
 import './App.css';
 
 class App extends Component {
@@ -11,6 +15,7 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
+          <NavBar />
           <div id="page-body">
             <Route path="/" component={HomePage} exact />
             <Route path="/about" component={AboutPage} />
