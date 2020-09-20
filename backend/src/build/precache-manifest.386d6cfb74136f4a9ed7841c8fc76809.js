c2abe7ee12f5e576e5f24c4b5bb100fc682228cd
Chacko <chackop@gmail.com>
2020-09-20 16:45:59 +0100

    added to single server

---
+++ backend/src/build/precache-manifest.386d6cfb74136f4a9ed7841c8fc76809.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72a0ead0a761698ce94e5580bd2f9d52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c862c8beef871416302",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "32c8a8b9bf9aed6267c0",
+    "url": "/static/js/2.53b28422.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.53b28422.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c862c8beef871416302",
+    "url": "/static/js/main.eaf2e443.chunk.js"
+  },
+  {
+    "revision": "6b47673d4ed41c144c61",
+    "url": "/static/js/runtime-main.cc876693.js"
+  }
+]);
