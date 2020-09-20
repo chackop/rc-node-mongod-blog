ba86f24d7a58c249b7eff95b553521c5705d0ddf
Chacko <chackop@gmail.com>
2020-09-20 16:19:40 +0100

    useffect

--- frontend/src/pages/ArticlePage.js
+++ frontend/src/pages/ArticlePage.js
@@ -1,26 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ArticlesList from '../components/ArticlesList';
 import NotFoundPage from './NotFoundPage';
 import articleContent from './article-content';
 
 const ArticlePage = ({ match }) => {
-    const name = match.params.name;
-    const article = articleContent.find(article => article.name === name);
+  const name = match.params.name;
+  const article = articleContent.find((article) => article.name === name);
 
-    if (!article) return <NotFoundPage />
+  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
 
-    const otherArticles = articleContent.filter(article => article.name !== name);
+  useEffect(() => {
+    setArticleInfo({ upvotes: Math.ceil(Math.random() * 10) });
+  }, [name]);
 
-    return (
-        <>
-        <h1>{article.title}</h1>
-        {article.content.map((paragraph, key) => (
-            <p key={key}>{paragraph}</p>
-        ))}
-        <h3>Other Articles:</h3>
-        <ArticlesList articles={otherArticles} />
-        </>
-    );
-}
+  if (!article) return <NotFoundPage />;
+
+  const otherArticles = articleContent.filter(
+    (article) => article.name !== name
+  );
+
+  return (
+    <>
+      <h1>{article.title}</h1>
+      <p>This post has been upvoted {articleInfo.upvotes} times</p>
+      {article.content.map((paragraph, key) => (
+        <p key={key}>{paragraph}</p>
+      ))}
+      <h3>Other Articles:</h3>
+      <ArticlesList articles={otherArticles} />
+    </>
+  );
+};
 
 export default ArticlePage;
